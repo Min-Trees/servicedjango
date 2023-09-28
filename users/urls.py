c3498e6c73188d750d1281c484240bf615e476a6
Min-Trees <2224802010382@student.tdmu.edu.vn>
2023-09-28 08:30:00 +0700

    gghg

--- users/urls.py
+++ users/urls.py
@@ -3,12 +3,12 @@ from .views import UserViewDetail, getUser,Profile, deleteUserView, getProfile
 import requests
 urlpatterns = [
     # return user
-    path('detail/<str:id>/',UserViewDetail.as_view(),name='user-detail'),
+    path('detail/<str:id>',UserViewDetail.as_view(),name='user-detail'),
     #http.... / api/detail/1
-    path('delete-user/<str:id>/',deleteUserView.as_view(),name='delete-user'),
+    path('delete-user/<str:id>',deleteUserView.as_view(),name='delete-user'),
     #get user from auth
-    path('getuser-from-auth/',getUser,name='getuser-from-auth'),
-    path('profile-user/<int:id>/',Profile.as_view(), name='profile-user'),
-    path('getuser-profile/', getProfile, name='getuser-profile'),
+    path('register',getUser,name='register'),
+    path('profile-user/<int:id>',Profile.as_view(), name='profile-user'),
+    path('getuser-profile', getProfile, name='getuser-profile'),
 
 ]
