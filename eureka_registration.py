b974950fba4994348b867161864f30e47510e5c3
Min-Trees <2224802010382@student.tdmu.edu.vn>
2023-11-26 22:36:27 +0700

    Resolve conflicts inyfgfg

---
+++ eureka_registration.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+# Địa chỉ IP và cổng của Eureka Server
+eureka_server_url = "https://tdmu-discovery-service.onrender.com/eureka/"
+
+# URL của dịch vụ Django của bạn
+django_service_url = ""
+
+# Headers cho request POST
+headers = {'Content-Type': 'application/json'}
+# Dữ liệu đăng ký dịch vụ
+eureka_payload = {
+    "instance": {
+        "hostName": "localhost",
+        "app": "USER-SERVICE",
+        "ipAddr": "10.220.9.136",  # Địa chỉ IP của Eureka Server
+        "vipAddress": "USER-SERVICE",
+        "secureVipAddress": "USER-SERVICE",
+        "status": "UP",
+        "port": {"$": 8000, "@enabled": "true"},
+        "securePort": {"$": 8443, "@enabled": "false"},
+        "homePageUrl": f"{django_service_url}/",
+        "statusPageUrl": f"{django_service_url}/status",
+        "healthCheckUrl": f"{django_service_url}/health",
+        "dataCenterInfo": {"@class": "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo", "name": "MyOwn"}
+    }
+}
+
+# Thực hiện request POST đến Eureka Server
+response = requests.post(f"{eureka_server_url}apps/{eureka_payload['instance']['app']}", data=json.dumps(eureka_payload), headers=headers)
+
+if response.status_code == 204:
+    print("Đăng ký dịch vụ thành công.")
+else:
+    print(f"Không thể đăng ký dịch vụ. Phản hồi: {response.text}")
